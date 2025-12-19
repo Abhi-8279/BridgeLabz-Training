9696c17d97905cffcec8813f293b73c2f34af71c
Abhishek Gupta <abhishek.gupta_cs22@gla.ac.in>
2025-12-19 15:30:22 +0530

    First Commit

---
+++ core-java-practice/gcr-codebase/programming-elements/DiscountCalculate8.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class DiscountCalculate8 {
+    public static void main(String[] args) {
+
+        double fee;
+        double discountPercent;
+        double discount;
+        double finalFee;
+
+        Scanner input = new Scanner(System.in);
+
+        fee = input.nextDouble();
+        discountPercent = input.nextDouble();
+
+        discount = (fee * discountPercent) / 100;
+        finalFee = fee - discount;
+
+        System.out.println(
+            "The discount amount is INR " + discount +
+            " and final discounted fee is INR " + finalFee
+        );
+    }
+}
+
